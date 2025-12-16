d5253557a35ad4e51b0c3b7d751a0f0924667c2c
YahyaHafidz <yahya.hafidz@amarbank.co.id>
2025-12-16 14:08:08 +0700

    import env and create test for login

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,4 +1,8 @@
 import {test, expect} from "@playwright/test";
+import dotenv from 'dotenv';
+import { LoginPage } from "../pages/login/loginPage";
+
+
 
 test.describe("Home page with no auth", () => {
   test.beforeEach(async ({ page }) => {
@@ -12,4 +16,18 @@ test.describe("Home page with no auth", () => {
     });
   });
 
+  
+  test("Login with page object", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.emailInput.fill(process.env.EMAIL!);
+  await loginPage.passwordInput.fill(process.env.PASSWORD!);
+  await loginPage.loginButton.click();
+  await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
+});
+
+
+
+
   });
+
